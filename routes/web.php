04104e800e5f1e7592db0220afb3627afd025c57
zhuanxuhit <893051481@qq.com>
2016-11-22 16:54:06 +0800

    add dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | to using a Closure or controller method. Build something great!
 |
 */
+//
+//Route::get('/', function () {
+//    return view('dashboard');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',"DashboardController@indexAction");
